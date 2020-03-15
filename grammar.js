1070425a6dfd4c8c4dba23fdf9b2a8e05f2f387d
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-03-15 17:26:44 -0600

    Formatting

--- grammar.js
+++ grammar.js
@@ -1,63 +1,53 @@
 module.exports = grammar({
   name: "webassembly",
 
-  inline: $ => [
-  ],
+  inline: $ => [],
 
   rules: {
-    file: $ => choice(
-      $.module,
-    ),
+    file: $ => choice($.module),
 
     _bindVar: $ => $.VAR,
 
     expr: $ => "expr",
 
-    func: $ => seq(
-      "(",
-      "func",
-      optional($._bindVar),
-      optional($.funcField),
-      ")",
-    ),
+    func: $ => seq("(", "func", optional($._bindVar), optional($.funcField), ")"),
 
-    funcBody: $ => choice(
-      repeat1($.instr),
-      //
-      //
-    ),
+    funcBody: $ =>
+      choice(
+        repeat1($.instr),
+        //
+        //
+      ),
 
-    funcField: $ => choice(
-      seq($.typeUse, optional($.funcFieldBody)),
-      seq($.inlineImport, $.typeUse, $.funcFieldImport),
-      seq($.inlineImport, $.funcFieldImport),
-      seq($.inlineExport, $.funcField),
-    ),
+    funcField: $ =>
+      choice(
+        seq($.typeUse, optional($.funcFieldBody)),
+        seq($.inlineImport, $.typeUse, $.funcFieldImport),
+        seq($.inlineImport, $.funcFieldImport),
+        seq($.inlineExport, $.funcField),
+      ),
 
-    funcFieldBody: $ => choice(
-      $.funcResultBody,
-      //
-      //
-    ),
+    funcFieldBody: $ =>
+      choice(
+        $.funcResultBody,
+        //
+        //
+      ),
 
     funcFieldImport: $ => "funcFieldImport",
 
-    funcResultBody: $ => choice(
-      $.funcBody,
-      //
-      //
-    ),
+    funcResultBody: $ =>
+      choice(
+        $.funcBody,
+        //
+        //
+      ),
 
     inlineExport: $ => "inlineExport",
 
     inlineImport: $ => "inlineImport",
 
-    instr: $ => choice(
-      $.instrCall,
-      $.instrPlain,
-      $.instrBlock,
-      $.expr,
-    ),
+    instr: $ => choice($.instrCall, $.instrPlain, $.instrBlock, $.expr),
 
     instrCall: $ => "instrCall",
 
@@ -65,28 +55,13 @@ module.exports = grammar({
 
     instrBlock: $ => "instrBlock",
 
-    module: $ => choice(
-      seq(
-        "(",
-        "module",
-        optional($._moduleVar),
-        repeat($.moduleField),
-        ")",
-      ),
-    ),
+    module: $ => choice(seq("(", "module", optional($._moduleVar), repeat($.moduleField), ")")),
 
-    moduleField: $ => choice(
-      $.func,
-    ),
+    moduleField: $ => choice($.func),
 
     _moduleVar: $ => $.VAR,
 
-    typeUse: $ => seq(
-      "(",
-      "type",
-      $._var,
-      ")",
-    ),
+    typeUse: $ => seq("(", "type", $._var, ")"),
 
     // terminal
 
@@ -103,31 +78,19 @@ module.exports = grammar({
 
     _hexdigit: $ => /[0-9a-fA-F]/,
 
-    _hexnum: $ => seq(
-      $._hexdigit,
-      repeat(seq(optional("_"), $._hexdigit)),
-    ),
+    _hexnum: $ => seq($._hexdigit, repeat(seq(optional("_"), $._hexdigit))),
 
     _name: $ => {
-      const digit  = /[0-9]/;
+      const digit = /[0-9]/;
       const letter = /[a-zA-Z]/;
       const symbol = /[-+*/\\^~=<>!?@#$%&|:`.']/;
       return seq("$", repeat1(choice(letter, digit, "_", symbol)));
     },
 
-    _nat: $ => choice(
-      $._num,
-      seq("0x", $._hexnum),
-    ),
+    _nat: $ => choice($._num, seq("0x", $._hexnum)),
 
-    _num: $ => seq(
-      $._digit,
-      repeat(seq(optional("_"), $._digit)),
-    ),
+    _num: $ => seq($._digit, repeat(seq(optional("_"), $._digit))),
 
-    _var: $ => choice(
-      $.NAT,
-      $.VAR,
-    ),
+    _var: $ => choice($.NAT, $.VAR),
   },
-})
+});
