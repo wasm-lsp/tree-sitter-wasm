288f8e898d6d34e79b9b4aeb21ac0c9f9a307210
silvanshade <silvanshade@users.noreply.github.com>
2020-03-31 15:00:26 -0600

    Adjust wast grammar to parse top-level commands

--- wast/grammar.js
+++ wast/grammar.js
@@ -2,4 +2,10 @@ const wat = require("../wat/grammar");
 
 module.exports = grammar(wat, {
   name: "wast",
+
+  rules: {
+    ENTRYPOINT: $ => repeat($.cmd),
+
+    cmd: $ => choice($.module),
+  },
 });
