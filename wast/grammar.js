79e4b4af7bc9d8f7d1107edb397acc751028433f
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-04-09 12:59:32 -0600

    Rewrite grammars

--- wast/grammar.js
+++ wast/grammar.js
@@ -4,107 +4,89 @@ module.exports = grammar(wat, {
   name: "wast",
 
   rules: {
-    ENTRYPOINT: $ => repeat(field("command", $.command)),
+    PARSE: $ => choice(repeat(field("command", $.command)), alias(repeat1($._module_field), $.inline_module)),
 
-    // ====================================================== //
-    // ======================= Script ======================= //
-    // ====================================================== //
+    _literal_nan: $ => choice($.literal_nan_arithmetic, $.literal_nan_canonical),
 
-    // NOTE: we inline this because it matches the empty string
-    // script: $ => repeat($.command),
+    literal_nan_arithmetic: $ => "nan:arithmetic",
 
-    // ====================================================== //
-    // ====================== Commands ====================== //
-    // ====================================================== //
+    literal_nan_canonical: $ => "nan:canonical",
 
-    command: $ => choice($._module_ext, $.register, $._action, $._assertion, $._meta),
+    _expr_plain_const: $ => seq("(", $.instr_plain_const, ")"),
 
-    register: $ => seq("(", "register", field("name", $.name), field("id", optional($.id)), ")"),
-
-    // ====================================================== //
-    // ================== Module Extensions ================= //
-    // ====================================================== //
-
-    _module_ext: $ => choice($.module, $._modulefield, $.module_binary, $.module_quote),
-
-    module_binary: $ => seq("(", $.MODULE, optional(field("id", $.id)), "binary", repeat(field("data", $.string)), ")"),
-
-    module_quote: $ => seq("(", $.MODULE, optional(field("id", $.id)), "quote", repeat(field("data", $.string)), ")"),
-
-    // ====================================================== //
-    // ======================= Actions ====================== //
-    // ====================================================== //
-
-    _action: $ => choice($.action_get, $.action_invoke),
-
-    action_get: $ => seq("(", "get", optional(field("id", $.id)), field("name", $.name), ")"),
-
-    action_invoke: $ =>
-      seq("(", "invoke", optional(field("id", $.id)), field("name", $.name), repeat(field("instr", $._instr)), ")"),
-
-    // ====================================================== //
-    // ===================== Assertions ===================== //
-    // ====================================================== //
-
-    _assertion: $ =>
-      choice(
-        $.assertion_return,
-        $.assertion_trap_action,
-        $.assertion_exhaustion,
-        $.assertion_malformed,
-        $.assertion_invalid,
-        $.assertion_unlinkable,
-        $.assertion_trap_module,
-      ),
-
-    assertion_return: $ =>
-      seq("(", "assert_return", field("action", $._action), field("result", repeat($.result_value)), ")"),
-
-    assertion_trap_action: $ => seq("(", "assert_trap", field("action", $._action), field("failure", $.string), ")"),
-
-    assertion_exhaustion: $ =>
-      seq("(", "assert_exhaustion", field("action", $._action), field("failure", $.string), ")"),
-
-    assertion_malformed: $ =>
-      seq("(", "assert_malformed", field("module", $._module_ext), field("failure", $.string), ")"),
-
-    assertion_invalid: $ => seq("(", "assert_invalid", field("module", $._module_ext), field("failure", $.string), ")"),
-
-    assertion_unlinkable: $ =>
-      seq("(", "assert_unlinkable", field("module", $._module_ext), field("failure", $.string), ")"),
-
-    assertion_trap_module: $ =>
-      seq("(", "assert_trap", field("module", $._module_ext), field("failure", $.string), ")"),
-
-    // ====================================================== //
-    // ======================= Results ====================== //
-    // ====================================================== //
-
-    result_value: $ =>
+    _expr_plain_const_nan: $ =>
       seq(
         "(",
-        field("const", choice($.I32_CONST, $.I64_CONST, $.F32_CONST, $.F64_CONST)),
-        field("numpat", $._numpat),
+        field("type", $._instr_type),
+        token.immediate("."),
+        field("op", alias(token.immediate(/const/), $.op)),
+        field("literal", $._literal_nan),
         ")",
       ),
 
-    _numpat: $ => choice($.iN, $.fN, $.numpat_nan_canonical, $.numpat_nan_arithmetic),
+    result: $ => choice($._expr_plain_const, $._expr_plain_const_nan),
 
-    numpat_nan_canonical: $ => "nan:canonical",
+    command: $ => choice($._action, $._assertion, $._meta, $.register, $._script_module),
 
-    numpat_nan_arithmetic: $ => "nan:arithmetic",
+    _action: $ => choice($.action_invoke, $.action_get),
 
-    // ====================================================== //
-    // ==================== Meta-Commands =================== //
-    // ====================================================== //
+    action_invoke: $ =>
+      seq(
+        "(",
+        "invoke",
+        optional(field("identifier", $.identifier)),
+        field("name", $.name),
+        repeat(field("const", $._expr_plain_const)),
+        ")",
+      ),
+
+    action_get: $ => seq("(", "get", optional(field("identifier", $.identifier)), field("name", $.name), ")"),
+
+    _assertion: $ =>
+      choice(
+        $.assert_exhaustion,
+        $.assert_invalid,
+        $.assert_malformed,
+        $.assert_return,
+        $.assert_trap_action,
+        $.assert_trap_module,
+        $.assert_unlinkable,
+      ),
+
+    assert_exhaustion: $ => seq("(", "assert_exhaustion", field("action", $._action), field("string", $.string), ")"),
+
+    assert_invalid: $ => seq("(", "assert_invalid", field("module", $._script_module), field("string", $.string), ")"),
+
+    assert_malformed: $ =>
+      seq("(", "assert_malformed", field("module", $._script_module), field("string", $.string), ")"),
+
+    assert_return: $ => seq("(", "assert_return", field("action", $._action), repeat(field("result", $.result)), ")"),
+
+    assert_trap_action: $ => seq("(", "assert_trap", field("action", $._action), field("string", $.string), ")"),
+
+    assert_trap_module: $ => seq("(", "assert_trap", field("module", $._script_module), field("string", $.string), ")"),
+
+    assert_unlinkable: $ =>
+      seq("(", "assert_unlinkable", field("module", $._script_module), field("string", $.string), ")"),
 
     _meta: $ => choice($.meta_script, $.meta_input, $.meta_output),
 
     meta_script: $ =>
-      seq("(", "script", optional(field("id", $.id)), field("script", alias(repeat($.command), "script")), ")"),
+      seq("(", "script", optional(field("identifier", $.identifier)), repeat(field("command", $.command)), ")"),
 
-    meta_input: $ => seq("(", "input", optional(field("id", $.id)), field("file", $.string), ")"),
+    meta_input: $ => seq("(", "input", optional(field("identifier", $.identifier)), field("string", $.string), ")"),
 
-    meta_output: $ => seq("(", "output", optional(field("id", $.id)), optional(field("file", $.string)), ")"),
+    meta_output: $ =>
+      seq("(", "output", optional(field("identifier", $.identifier)), optional(field("string", $.string)), ")"),
+
+    register: $ => seq("(", "register", field("name", $.name), optional(field("identifier", $.identifier)), ")"),
+
+    _script_module: $ => choice($.module, $.script_module_binary, $.script_module_quote),
+
+    script_module_binary: $ =>
+      seq("(", "module", optional(field("identifier", $.identifier)), "binary", repeat(field("string", $.string)), ")"),
+
+    script_module_quote: $ =>
+      seq("(", "module", optional(field("identifier", $.identifier)), "quote", repeat(field("string", $.string)), ")"),
   },
 });
