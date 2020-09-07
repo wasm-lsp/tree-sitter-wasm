07724c5afcc7e9f168dc81b0bc232e55b0dfe9b1
silvanshade <silvanshade@users.noreply.github.com>
2020-09-07 14:22:18 -0600

    Stop using git-lfs

--- witx/index.js
+++ witx/index.js
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c4e7c39775bd731fa1ebe147121f19a0afbb2abdaa500022ac174539cbdffc35
-size 348
+try {
+  module.exports = require("./build/Release/tree_sitter_webassembly_api_description_binding");
+} catch (error) {
+  try {
+    module.exports = require("./build/Debug/tree_sitter_webassembly_api_description_binding");
+  } catch (_) {
+    throw error
+  }
+}
+
+try {
+  module.exports.nodeTypeInfo = require("./src/node-types.json");
+} catch (_) {}
